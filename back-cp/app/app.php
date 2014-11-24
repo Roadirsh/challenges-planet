8781fe8eb7482d552a88276fa54992cc85af0895
Saskia Giraud <saskia.giraud@gmail.com>
2014-11-24 14:15:56 +0100

    v2.2 modification bastien

--- back-cp/app/app.php
+++ back-cp/app/app.php
@@ -22,7 +22,6 @@
 		$module = "User";
 		$urlController = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'controller/' . $module . 'Controller.php';
 	}
-
 	/**
 	 * Include des librairies du Core
 	 * @param String $module
@@ -31,14 +30,12 @@
 		include_once '../core/Load.php';
 	// Controller Globale
 		include_once '../core/CoreController.php';
-
-
-	// Lancement du module
-		$controller = $module . 'Controller';
-
-		new $controller();
+		
 	// Le controller appelé par la variable $module
 		include_once('controller/' . $module . 'Controller.php');
-
-
+	// Lancement du module
+		$controller = $module . 'Controller';
+		
+		$index = new UserController();
+		$index->Login();
 		
--- back-cp/public/index.php
+++ back-cp/public/index.php
@@ -8,23 +8,27 @@
  */
 
 
-// Demmarage de la session
-	session_start();
-// Nommage de la session
-	session_name('ChallengesPlanet');
 
+// Demmarage de la session
+    session_start();
+
+// Nommage de la session
+    session_name('ChallengesPlanet');
 
 // Fichier de configuration principal
-	require_once('../app/conf/conf_define.php');
+    require_once('../app/conf/conf_define.php');
+
 // Parametrage de l'UTF8 
-	header('Content-type: text/html; charset=UTF-8');
+    //header('Content-type: text/html; charset=UTF-8');
+
 // Ensemble des urls en dur
-	include(ROOT . "conf/conf_url.php");
+    include(ROOT . "conf/conf_url.php");
+
 // Librairie principale et globale du framework
-	include("../lib/lib.php");
+    include("../lib/lib.php");
 
 // Affichage des erreurs php
-	error_reporting(E_ALL);
-
+    error_reporting(E_ALL);
+    
 // Lancement de l'application !!!!!
     include_once(ROOT . 'app.php');
