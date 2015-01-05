ce9491ae732a16b79e4636a7d45fcf48bcf23119
Saskiagi <saskia.giraud@gmail.com>
2015-01-05 13:10:20 +0100

    commentaires + début one user

---
+++ back-cp/app/view/user/seeOneUser.php
@@ -0,0 +1,12 @@
+<?php // $logger->log('test', 'loadapp', "Chargement de la vue home.php", Logger::GRAN_MONTH); ?>
+<? include(ROOT . "view/layout/header.inc.php"); ?>
+<? include(ROOT . "view/layout/menutop.inc.php"); ?>
+<? include(ROOT . "view/layout/menu.inc.php"); ?>
+
+<div id="content" class="span10">
+    <div class="row-fluid">		
+				
+	</div><!--/row-->
+</div>
+
+<?php include(ROOT . "view/layout/footer.inc.php"); ?>
