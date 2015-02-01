2ba46ad64d74b26d02e7018cb92a39bd87a024c4
pookiz <aurelie.daguanno@gmail.com>
2015-02-01 15:29:57 +0100

    update seeEvent responsive (search & sort lightbox), new page : event.php (teams list of the event) rename like as you want Saskia

--- front-cp/app/view/event/seeEvent.php
+++ front-cp/app/view/event/seeEvent.php
@@ -20,7 +20,65 @@
 	</div>
 
 	<div class="main-contain clearfix">
-		<div class="sidebar-sort medium-3 columns">
+		<a href="#form-search" class="hide-for-large-up show-for-small-up popup-with-form sidebar-sort-mobile">Search and sort</a>
+		<form id="form-search" class="search-bar mfp-hide white-popup-block" method="posst" action="">
+			<input type="text" size="21">
+				
+				<div class="number-teams">
+					<span>Number of teams</span>
+					<div <?php if(isset($nbteam) && $nbteam =='1-15'){ ?> class="active" <?php }?>>
+						<input type="checkbox" name="nb_team" value="1-15" onclick='submit();'> 1-15
+					</div>
+					<div <?php if(isset($nbteam) && $nbteam =='15-50'){ ?> class="active" <?php }?>>
+						<input type="checkbox" name="nb_team" value="15-50" onclick='submit();'> 15-50
+					</div>
+					<div <?php if(isset($nbteam) && $nbteam =='+50'){ ?> class="active" <?php }?>>
+						<input type="checkbox" name="nb_team" value="+50" onclick='submit();'> More than 50
+					</div>    
+				</div>
+				
+				<div class="begin-race">
+					<span>Beginning of the race</span>
+					<div>
+						<input type="checkbox" name="begin" value="-1 week" onclick='submit();'> Less of one week
+					</div>
+					<div>
+						<input type="checkbox" name="begin" value="2-3 week" onclick='submit();'> 2-3 weeks
+					</div>
+					<div>
+						<input type="checkbox" name="begin" value="+1month" onclick='submit();'> More than one month
+					</div>
+					<div>
+						<input type="checkbox" name="begin" value="+6month" onclick='submit();'> More than 6 months
+					</div>
+				</div>
+
+				<div class="kind-race">
+					<span>Kind of race</span>
+					<div <?php if(isset($tpe) && $tpe =='earth'){ ?> class="active" <?php }?>>
+						<input type="checkbox" name="type" value="earth" onchange='submit();'> Earth
+					</div>
+					<div <?php if(isset($tpe) && $tpe  =='sea'){ ?> class="active" <?php }?>>
+						<input type="checkbox" name="type" value="sea" onchange='submit();'> Sea
+					</div>
+					<div <?php if(isset($tpe) && $tpe  =='air'){ ?> class="active" <?php }?>>
+						<input type="checkbox" name="type" value="air" onchange='submit();'> Air
+					</div>
+					<div <?php if(isset($tpe) && $tpe  =='car'){ ?> class="active" <?php }?>>
+						<input type="checkbox" name="type" value="car" onchange='submit();'> Car
+					</div>
+					<div <?php if(isset($tpe) && $tpe  =='boat'){ ?> class="active" <?php }?>>
+						<input type="checkbox" name="type" value="boat" onchange='submit();'> Boat
+					</div>
+					<div <?php if(isset($tpe) && $tpe  =='surf'){ ?> class="active" <?php }?>>
+						<input type="checkbox" name="type" value="surf" onchange='submit();'> Surf
+					</div>
+					<div <?php if(isset($tpe) && $tpe  =='bike'){ ?> class="active" <?php }?>>
+						<input type="checkbox" name="type" value="bike" onchange='submit();'> Bike
+					</div>
+				</div>
+		</form>
+		<div class="show-for-large-up sidebar-sort medium-3 columns">
 			<h2>Search and sort</h2>
 			<form class="search-bar" method="post" action="">
 				<input type="text" size="21">
@@ -80,20 +138,20 @@
 				</div>
 			</form>
 		</div>
-		<div class="events medium-9 clearfix columns">
+		<div class="events large-9 medium-12 clearfix columns">
 		<?php if(isset($events[0]) && !empty($events[0]) ){ ?>
 		<?php foreach($events as $k => $e){ ?>
 				<div class="event medium-6 columns">
 					<div class="wrapper clearfix">
-						<div class="img medium-6 columns">
+						<div class="img large-6 medium-12 columns">
 							<img src="img/event/<?php echo $e['event_img']; ?>">
 						</div>
-						<div class="infos medium-6 columns">
+						<div class="infos large-6 medium-12 columns">
 							<h2><?php echo $e['event_name']; ?></h2>
 							<span><b><?php echo $e['event_type']; ?></b> race</span>
 							<span class="date"><?php echo formDate($e['event_begin'], 0); ?> - <?php echo formDate($e['event_end'], 0); ?></span>
 						</div>
-						<div class="medium-6 columns teams">
+						<div class="medium-12 large-6 columns teams">
 						    <?php if($e['event_nb_team'] == 0){ $team = 'no teams yet'; } ?>
 						    <?php if($e['event_nb_team'] == 1){ $team = $e['event_nb_team'] . ' team'; } ?>
 						    <?php if($e['event_nb_team'] > 1){ $team = $e['event_nb_team'] . ' teams'; } ?>
@@ -106,14 +164,14 @@
 			<?php } else { ?>
 			    <div class="event medium-6 columns">
 					<div class="wrapper clearfix">
-						<div class="img medium-6 columns">
+						<div class="img large-6 medium-12 columns">
 							<img src="img/gogo.gif">
 						</div>
-						<div class="infos medium-6 columns">
+						<div class="infos large-6 medium-12 columns">
 							<h2>Sorry</h2>
 							<span>No events for this request</span>
 						</div>
-						<div class="medium-6 columns teams">
+						<div class="medium-12 large-6 columns teams">
 							<a href="<?php echo MODULE . 'event' . ACTION . 'seeevent' ?>" class="button-submit">Back to all events</a>
 						</div>
 					</div>
@@ -121,10 +179,10 @@
             <?php } ?>
 			<div class="event medium-6 columns">
 				<div class="wrapper clearfix">
-					<div class="img medium-6 columns">
+					<div class="img large-6 medium-12 columns">
 						<img src="img/ready.jpg" alt="">
 					</div>
-					<div class="medium-6 columns adventure">
+					<div class="medium-12 large-6 columns adventure">
 						<a href="<?php echo MODULE . 'event' . ACTION . 'addevent'; ?>" class="button-submit">Ready for your adventure ?</a>
 					</div>
 				</div>
