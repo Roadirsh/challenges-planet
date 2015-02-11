974813aea1f48f8e61f11a13eb2da2c055d96806
Saskiagi <saskia.giraud@gmail.com>
2015-02-11 14:13:59 +0100

    pic v1

--- front-cp/app/view/page/home.php
+++ front-cp/app/view/page/home.php
@@ -50,7 +50,7 @@
                     <div class="columns large-3 medium-4 team-wrapper">
         				<div class="wrapper">
         					<div class="img">
-        						<img src="img/group/<?php echo $t['group_img']; ?>" alt="">
+        						<img src="<?php echo PROJECT . $t['group_img']; ?>" alt="">
         						<div class="hover"><span>They need you<br>help them</span></div>
         					</div>
         					<div class="title-team"><a href="<?php echo MODULE . 'event' . ACTION . 'seeoneevent' . ID . $t['group_id']; ?>" alt="" title=""><?php echo $t['group_name']; ?></a></div>
