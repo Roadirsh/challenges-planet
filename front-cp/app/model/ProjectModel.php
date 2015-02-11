1c2a2936a47b8dad2e602dedc85c810ee014e474
Bastien Ledon <bastien.ledon@yahoo.com>
2015-02-11 22:08:38 +0100

    fonction pour l'application

--- front-cp/app/model/ProjectModel.php
+++ front-cp/app/model/ProjectModel.php
@@ -24,14 +24,27 @@ class ProjectModel extends CoreModel{
     public function getProjectJSON() {
 
         try {
-
-                $select = $this->connexion->prepare("SELECT group_name, group_descr, group_id 
-                                                    FROM  `cp_group` 
-                                                    LEFT JOIN cp_event_has_group 
-                                                    ON cp_group.group_id = cp_event_has_group.group_group_id  
-                                                    WHERE `cp_event_has_group`.`event_event_id` = :id");
-
-                $select->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+				$selectfrom = "SELECT group_name, group_descr, group_id 
+                                                    FROM  `cp_group` ";
+                if(isset($_GET['id'])){
+	                $projetparevent = "`cp_event_has_group`.`event_event_id` = :id AND ";
+					$jointure = "LEFT JOIN cp_event_has_group 
+                             ON cp_group.group_id = cp_event_has_group.group_group_id ";
+							 
+                }
+                else{
+	                $jointure = "";
+	                $projetparevent = "";
+                }
+                
+                
+                $where = "WHERE "  .  $projetparevent . " group_valid = 1";
+                $query = $selectfrom.$jointure.$where;
+                                                    
+                $select = $this->connexion->prepare($query);
+				if(isset($_GET['id'])){
+                	$select->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+                }
                 $select->execute();
                 $select->setFetchMode(PDO::FETCH_ASSOC);
                 $event = $select->FetchAll();
