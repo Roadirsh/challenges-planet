8bb4352e982b43491bb293eb5bff6c07735f4dab
pookiz <aurelie.daguanno@gmail.com>
2015-02-20 17:08:02 +0100

    fix 404

--- front-cp/app/view/layout/notfound.php
+++ front-cp/app/view/layout/notfound.php
@@ -1,13 +1,9 @@
 <?php include("header.inc.php"); ?>
 
-    <div class="container row">
-        <div class="medium-12 center">
-			<div id="notfound">
-				<img src="img/logo404.png" alt="logo" title="logo"/>
-				<h2><span class="blue">404</span> PAGE NOT FOUND... <br/><span class="green">TOO BAD</span> :'(</h2>
-				<h3>you can <a href="<?php echo MODULE . 'page' . ACTION . 'home'; ?>" class="green">return to the home page</a></h3>
-			</div>
-        </div>
+	<div id="notfound">
+		<img src="img/logo404.png" alt="logo" title="logo"/>
+		<h2><span class="blue">404</span> PAGE NOT FOUND... <br/><span class="green">TOO BAD</span> :'(</h2>
+		<h3>you can <a href="<?php echo MODULE . 'page' . ACTION . 'home'; ?>" class="green">return to the home page</a></h3>
 	</div>
 
 <?php include("footer.inc.php"); ?>
