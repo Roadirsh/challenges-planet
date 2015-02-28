5bc74e63e8557e7b520fec0764baa9861a979290
Bastien Ledon <bastien.ledon@yahoo.com>
2015-02-28 06:11:07 +0100

    Cadeau Aurélie

--- front-cp/app/view/page/home.php
+++ front-cp/app/view/page/home.php
@@ -172,5 +172,25 @@
 		</div>
 	</a>		
 </div>
+<audio src="../../front-cp/public/img/user.mp3" style="" id="hoverSound">
+		Your browser does not support the audio element.
+</audio><script type="text/javascript">
+	jQuery(function(){
+    var kKeys = [];
+    function Kpress(e){
+        kKeys.push(e.keyCode);
+        if (kKeys.toString().indexOf("38,38,40,40,37,39,37,39,66,65") >= 0) {
+            jQuery(this).unbind('keydown', Kpress);
+            kExec();
+        }
+    }
+    jQuery(document).keydown(Kpress);
+});
+function kExec(){
+   document.getElementById('hoverSound').play();
+   
+   
+}
+</script>
 
 <?php include(ROOT . "view/layout/footer.inc.php"); ?>
